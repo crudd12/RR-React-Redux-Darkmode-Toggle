f15e38df6d2ea83eba07eb285744ab495168a0d7
crudd12 <crudd12@gmail.com>
2024-04-18 20:08:16 -0700

    added slice and store files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,31 @@ import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
 
+//connect to data
+import { useSelector, useDispatch, connect } from 'react-redux';
+
+//mutate data
+import { setDarkTheme, setLightTheme } from './features/themeSlice';
+
 function App() {
-  
+  const dispatch = useDispatch()
+  //select a chunk of state from redux
+  const theme = useSelector((state) => state.theme)
+
+  console.log("state from redux", theme)
+
+  const handleOnClick = () => {
+    if (theme.darkMode) {
+      dispatch(setLightTheme())
+    }
+    else {
+      dispatch(setDarkTheme())
+    }
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: theme.color1, color: theme.color5 }} className="App">
+      <button onClick={handleOnClick}>Set Theme</button>
       <Nav />
       <ContentWrapper />
       <Footer />
